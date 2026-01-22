b9c6d5cb297c2853de5dfd948efbfc7b22e4adda
patel9619ali <patel9619ali@gmail.com>
2026-01-22 21:22:21 +0530

    Verifying token

--- app/(auth)/new-verification/page.tsx
+++ app/(auth)/new-verification/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
 import { NewVerificationForm } from "@/components/NewVerificationForm/NewVerificationForm";
+
 export default function NewVerificationPage() {
-    return (
-        <NewVerificationForm />
-    );
-  }
-  
+  return (
+    <Suspense fallback={<div>Verifying...</div>}>
+      <NewVerificationForm />
+    </Suspense>
+  );
+}
--- components/NewVerificationForm/NewVerificationForm.tsx
+++ components/NewVerificationForm/NewVerificationForm.tsx
@@ -1,11 +1,44 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import { Loader } from "lucide-react";
 import { Card } from "../ui/card";
+import { useCallback, useEffect,useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "../auth/FormSuccess";
+import { FormError } from "../auth/FormError";
 export const NewVerificationForm = () => {
+const [error, setError] = useState<string | undefined>();
+const [success, setSuccess] = useState<string | undefined>();
+const searchParams = useSearchParams();
+const token = searchParams.get("token");
+
+const onSubmit = useCallback(() => {
+  if (!token) {
+    setError("Missing token!");
+    return;
+  }
+  newVerification(token)
+    .then((data) => {
+      if (!data?.error) {
+        setSuccess("Verification successful!");
+      }
+      setError(data?.error);
+    })
+    .catch(() => {
+      setError("Something went wrong!");
+    });
+}, [token]);
+useEffect(() => {
+onSubmit();
+    // Here you can add logic to verify the token with your backend
+}, [onSubmit]);
+
 return (
     <Card className="w-full max-w-md p-6">
         <div className="flex items-center w-full justify-center">
-            <Loader />
+            {!success && !error && <Loader />}
+            <FormSuccess message={success}/>
+            <FormError message={error}/>
         </div>
     </Card>
 );
