093c01aff37fa1f345e869edb7b4d2ce7dc6d692
patel9619ali <patel9619ali@gmail.com>
2026-01-23 21:57:52 +0530

    Use search params with suspense boundry implemented

--- app/(auth)/new-password/page.tsx
+++ app/(auth)/new-password/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
+import { NewVerificationForm } from "@/components/NewVerificationForm/NewVerificationForm";
 import { NewPasswordForm } from "@/components/NewPasswordForm/NewPasswordForm";
 
-const NewPassword = () => {
-  return <NewPasswordForm />;
-};
-
-export default NewPassword;
+export default function NewPassword() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewPasswordForm/>
+    </Suspense>
+  );
+}
